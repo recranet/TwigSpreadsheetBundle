7efaa570cd1ffc045921a21c338ee4474e676748
Raymon de Looff <raymon@recranet.com>
2023-08-25 15:58:32 +0200

    Update php-cs-fixer configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,6 +1,10 @@
 <?php
 
-return PhpCsFixer\Config::create()
+if (!file_exists(__DIR__.'/src')) {
+    exit(0);
+}
+
+return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@Symfony' => true,
@@ -8,26 +12,34 @@ return PhpCsFixer\Config::create()
         'array_syntax' => ['syntax' => 'short'],
         'combine_consecutive_unsets' => true,
         // one should use PHPUnit methods to set up expected exception instead of annotations
-        'general_phpdoc_annotation_remove' => ['expectedException', 'expectedExceptionMessage', 'expectedExceptionMessageRegExp'],
+        'general_phpdoc_annotation_remove' => [
+            'annotations' => ['expectedException', 'expectedExceptionMessage', 'expectedExceptionMessageRegExp'],
+        ],
         'heredoc_to_nowdoc' => true,
-        'no_extra_consecutive_blank_lines' => ['break', 'continue', 'extra', 'return', 'throw', 'use', 'parenthesis_brace_block', 'square_brace_block', 'curly_brace_block'],
-        'no_short_echo_tag' => true,
+        'echo_tag_syntax' => true,
+        'no_extra_blank_lines' => [
+            'tokens' => ['break', 'continue', 'extra', 'return', 'throw', 'use', 'parenthesis_brace_block', 'square_brace_block', 'curly_brace_block'],
+        ],
+        'no_superfluous_phpdoc_tags' => false,
         'no_unreachable_default_argument_value' => true,
         'no_useless_else' => true,
         'no_useless_return' => true,
         'ordered_class_elements' => true,
         'ordered_imports' => true,
-        'php_unit_strict' => true,
-        'phpdoc_add_missing_param_annotation' => true,
+        'phpdoc_add_missing_param_annotation' => [
+            'only_untyped' => false,
+        ],
         'phpdoc_order' => true,
         'phpdoc_var_without_name' => false,
         'semicolon_after_instruction' => true,
         'strict_comparison' => true,
         'strict_param' => true,
+        'yoda_style' => false,
     ])
     ->setFinder(
         PhpCsFixer\Finder::create()
-            ->exclude('var')
-            ->in(__DIR__)
+            ->in(__DIR__.'/src')
+            ->in(__DIR__.'/tests')
+            ->append([__FILE__])
     )
 ;
